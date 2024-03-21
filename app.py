6ef630e68915bb8ed586aa143a63a4e44f3ea744
Mateusz Szewczyk <mateusz.szewczyk000@gmail.com>
2024-03-21 22:34:31 +0100

    Add css and html

--- app.py
+++ app.py
@@ -1,15 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template, request
+
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def main_page():
-    return 'Hello World!'
-
+def index():
+    data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    return render_template('index.html', data=data)
 
 if __name__ == '__main__':
-    app.run()
-
-
+    app.run(debug=True)
 
