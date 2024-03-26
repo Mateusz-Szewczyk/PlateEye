d75b656900e386460fc7d5d4910a10010c57715d
Mateusz Szewczyk <mateusz.szewczyk000@gmail.com>
2024-03-26 20:54:44 +0100

    add create_image_dir and write_to_csv_file, to create directories to save files and to save data about uploaded cars

---
+++ utils/saving.py
@@ -0,0 +1,41 @@
+import csv
+import os
+
+
+def create_image_dir(uploaded_file):
+    file_name = uploaded_file.name
+    uploaded_file_path = "uploaded-file-data/uploads"
+    csv_path = os.path.join(uploaded_file_path, "number_plates.csv")
+    image_dir = os.path.join(uploaded_file_path, file_name.split(".")[0])
+    if not os.path.exists(image_dir):
+        os.makedirs(image_dir)
+    if not os.path.exists(uploaded_file_path):
+        os.makedirs(uploaded_file_path)
+    with open(os.path.join(image_dir, file_name, ), "wb") as file:
+        file.write(uploaded_file.getbuffer())
+    return image_dir
+
+
+def write_to_csv_file(image_or_path, number_plate_list):
+    uploaded_file_data = "uploaded-file-data"
+    csv_path = os.path.join(uploaded_file_data, "number_plates.csv")
+
+    if not os.path.exists(uploaded_file_data):
+        os.makedirs(uploaded_file_data)
+
+    # save the image with the detected number plates
+
+    if not os.path.isfile(csv_path):
+        with open(csv_path, "w") as csv_file:
+            csv_writer = csv.writer(csv_file)
+            csv_writer.writerow(["id", "image", "bbox", "text"])
+
+    with open(csv_path, "a") as csv_file:
+        # create a writer object
+        csv_writer = csv.writer(csv_file)
+        # write the header
+        # loop over the `number_plate_list` list
+        for i, (box, text) in enumerate(number_plate_list):
+            # write the image path, bounding box coordinates,
+            # and detected text to the CSV file
+            csv_writer.writerow([i, image_or_path, box, text])
