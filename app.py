76d3fa8168fd73c7c4d5900168d237198d627ef9
Mateusz Szewczyk <mateusz.szewczyk000@gmail.com>
2024-03-26 20:46:59 +0100

    3 columns, original image, number plate detected image, processed image

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-import streamlit as st
-from ultralytics import YOLO
-import cv2
-from easyocr import Reader
 import os
-from detect_and_recognize import detect_number_plates, recognize_number_plates
+import cv2
+import streamlit as st
+
+from utils.anpr import detect_number_plates, recognize_number_plates, model_and_reader
+from utils.image_processing import prepare_images
+from utils.saving import create_image_dir
 
 st.set_page_config(page_title="Number Plate Detection and Recognition", page_icon="🚗", layout="wide")
 st.title("Number Plate Detection and Recognition :car:")
@@ -12,36 +13,29 @@ st.markdown("---")
 uploaded_file = st.file_uploader("Upload an Image", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
-    uploaded_file_data = "uploaded-file-data"
-    upload_image_path = os.path.join(uploaded_file_data, "uploads")
-    csv_path = os.path.join(uploaded_file_data, "number_plates.csv")
-    image_dir = os.path.join(upload_image_path, uploaded_file.name.split(".")[0])
-    if not os.path.exists(image_dir):
-        os.makedirs(image_dir)
-    if not os.path.exists(upload_image_path):
-        os.makedirs(upload_image_path)
-    with open(os.path.join(image_dir, uploaded_file.name, ), "wb") as file:
-        file.write(uploaded_file.getbuffer())
+    image_dir = create_image_dir(uploaded_file)
 
     with st.spinner("In progress..."):
-        model = YOLO("runs/detect/train/weights/best.pt")
-        reader = Reader(["en"], gpu=True)
-        image = cv2.cvtColor(cv2.imread(os.path.join(image_dir, uploaded_file.name)), cv2.COLOR_BGR2RGB)
-        image_copy = image.copy()
+        model, reader = model_and_reader()
+        image, image_copy = prepare_images(uploaded_file, image_dir)
 
-        col1, col2 = st.columns(2)
+        col1, col2, col3 = st.columns(3)
 
         with col1:
             st.subheader("Original Image")
             st.image(image)
 
-        number_plate_list = detect_number_plates(image, model)
-        if number_plate_list:
+        bounding_boxes_list = detect_number_plates(image, model)
+        if bounding_boxes_list:
             with col2:
                 st.subheader("Number Plate Detected Image")
                 st.image(image)
-            number_plate_list = recognize_number_plates(os.path.join(image_dir, uploaded_file.name), reader, number_plate_list)
-            for i, (box, text) in enumerate(number_plate_list):
+            bbox_and_number_plate_list, number_plates_img_list = recognize_number_plates(os.path.join(image_dir, uploaded_file.name), reader, bounding_boxes_list)
+            with col3:
+                st.subheader("Processed Image")
+                for i in range(min(4, len(number_plates_img_list))):
+                    st.image(number_plates_img_list[i])
+            for i, (box, text) in enumerate(bbox_and_number_plate_list):
                 cropped_number_plate = image_copy[box[1]:box[3], box[0]:box[2]]
                 cv2.rectangle(image, (box[0], box[1]), (box[2], box[3]), (0, 255, 0), 2)
                 cv2.putText(image, text, (box[0], box[3] + 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
